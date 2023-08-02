d6f8c34b3cf952b8c1936d539109a870c3df8a6b
Jaewoo Kim <wodndhksk@gmail.com>
2023-08-02 21:36:29 +0900

    실전 예제 5 - 연관관계 관리
    - @ManyToOne, @OneToOne fetchType을 Lazy로 설정(저 둘의 default = EAGER)
    - Order -> Delivery를 영속성 전이 ALL로 설정
    - Order -> OrderItem 영속성 전이 ALL로 설정

--- src/main/java/hellojpa/jpabook/jpashop/domain/Category.java
+++ src/main/java/hellojpa/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class Category extends BaseEntity {
     @Id @GeneratedValue
@@ -17,7 +19,7 @@ public class Category extends BaseEntity {
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
-    @ManyToMany
+    @ManyToMany(fetch = LAZY)
     @JoinTable(name = "CATEGORY_ITEM",
             joinColumns = @JoinColumn(name = "CATEGORY_ID"),
             inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
